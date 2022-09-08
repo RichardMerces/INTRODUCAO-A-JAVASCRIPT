600161f46c677b29caa28b526e68e898fa5625a0
RichardMerces <richardmerces@outlook.com>
2022-09-08 08:40:45 -0300

    Introducao a JS

---
+++ JS/anotacoes.js
@@ -0,0 +1,35 @@
+/*
+    JAVASCRIPT - CLIENT SIDE
+
+    HTML É ONDE EU COLOCO O MEU CÓDIGO EM JAVASCRIPT.
+
+    JS É POPULAR EM MAIS DE 90% DAS PÁGINAS DA INTERNET
+
+    CSS é onde eu estilizo a minha página HTML
+
+    JAVASCRIPT é onde eu dou funcionalidade para minha página
+
+    
+    console.log("Richard") -> Tem a mesma função do escreva do Portugol
+    
+    ------------------
+
+    DOM - Document Object Model
+    É lá que eu consigo estruturar todo o meu documento.
+
+    O HTML é o "que". o DOM é o "como".
+
+    --------------------
+    
+    VARIÁVEIS EM JAVASCRIPT
+
+    Existem 3 formas simples de se declarar variáveis em JS 
+
+    var & let = para variáveis que mudam com o tempo
+
+    const = para variáveis que serão sempre constantes
+
+    TIPAGEM DINÂMICA - O Javascript consegue reconhecer automáticamente o tipo de cada vatriável, sem que eu precise especificar nos mínimos detalhes
+
+    */
+
---
+++ PrimeiraQuestão/script.js
@@ -0,0 +1,11 @@
+const lampada = document.getElementById("lamp")
+
+function acender() 
+{
+    lampada.src = "lampOn.jpg"
+}
+
+function apagar()
+{
+    lampada.src = "lampOff.jpg"
+}
